4330a7c3db7367929169686a3f7c0e95acf9f81d
Aman Aggarwal <aggarwalaman1985@gmail.com>
2020-04-24 14:51:04 +0530

    Pushing code updated on April 2020

--- FacebookLogin-Swift/ViewController.swift
+++ FacebookLogin-Swift/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
 
 class ViewController: UIViewController {
 
@@ -15,6 +17,39 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func loginWithFacebook(_ sender: Any) {
+        let manager = LoginManager()
+        manager.logIn(permissions: [.publicProfile, .email], viewController: self) { (result) in
+            switch result {
+            case .cancelled:
+                print("User cancelled login process")
+                break
+            case .failed(let error) :
+                print("Login failed with error = \(error.localizedDescription)")
+                break
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                print("access token == \(accessToken)")
+                //self.getUserProfile()
+            }
+        }
+    }
+    
+    
+   /* func getUserProfile() {
+        let connection = GraphRequestConnection()
+        connection.add(GraphRequest(graphPath: "/me", parameters: ["fields" : "id,name,about,birthday"], accessToken: AccessToken.current, httpMethod: .GET, apiVersion: )) {
+            response, result in
+            switch result {
+            case .success(let response):
+                print("Logged in user facebook id == \(String(describing: response.dictionaryValue!["id"]))")
+                print("Logged in user facebook Name == \(String(describing: response.dictionaryValue!["name"]))")
+                break
+            case .failed(let error):
+                print("We havw error fetching loggedin user profile ==\(error.localizedDescription)")
+            }
+        }
+        connection.start()
+    }*/
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
